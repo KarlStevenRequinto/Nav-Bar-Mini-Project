cabf8d6630670ecac96ec7fb992bd60e8febdec8
Karl Steven <lerdrobott@gmail.com>
2021-08-10 07:01:41 +0800

    Day6:Commit1>Almost done!!!

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const listsbox = document.querySelector('.listsbox');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
     burger.addEventListener('click', () => {
-        nav.classList.toggle('listsbox-active');
+            //toggling nav
+        nav.classList.toggle('nav-active');
+            //links animation
+        navLinks.forEach((link, index) => {
+            
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
     });
+    //burger animation
+        burger.classList.toggle('toggle')    });
 }
 navSlide();
